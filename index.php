2973b4a6dd19cc993b7cf772c8af430c5f57ca76
Mike Prelesnik <mprelesnik@mail.greenriver.edu>
2019-01-14 13:09:00 -0500

    added an index page with a header and fat free instantiated

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+//Mike Prelesnik
+//1/11/19
+
+//turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once "vendor/autoload.php";
+
+//create an instance of the base class
+$f3 = Base::instance();
+
+//turn on fat-free error reporting
+$f3->set('DEBUG', 3);
+
+//define a default route
+$f3->route('GET /', function() {
+    echo "<h1>My Favorite Foods</h1>";
+    //$view = new View;
+    //echo $view->render('views/home.html');
+});
+
+//run fat-free
+$f3->run();
